ca4d3cd766a9c687a3185911272a1560c0ccbccf
Ralf Schimmel <r.schimmel@gynzy.com>
2016-07-18 10:02:57 +0200

    Update gcloud and use new env vars and implement request throttling

--- index.js
+++ index.js
@@ -7,66 +7,77 @@ var Promise = require('ember-cli/lib/ext/promise');
 var upload = require('./libs/upload');
 
 module.exports = {
-  name: 'ember-cli-deploy-gcloud-storage',
+	name: 'ember-cli-deploy-gcloud-storage',
 
-  createDeployPlugin: function(options) {
-    var Plugin = BasePlugin.extend({
-      name: options.name,
+	createDeployPlugin: function (options) {
+		var Plugin = BasePlugin.extend({
+			name: options.name,
 
-      defaultConfig: {
-        bucket: 'ember',
-        filePattern: '**/*.{js,css,png,gif,ico,jpg,map,xml,txt,svg,swf,eot,ttf,woff,woff2}',
-        distFiles: function(context) {
-          return context.distFiles || [];
-        },
-        gzippedFiles: function(context) {
-          return context.gzippedFiles || []; // e.g. from ember-cli-deploy-gzip
-        }
-      },
+			defaultConfig: {
+				bucket: 'ember',
+				filePattern: '**/*.{js,css,png,gif,ico,jpg,map,xml,txt,svg,swf,eot,ttf,woff,woff2}',
+				distFiles: function (context) {
+					return context.distFiles || [];
+				},
+				gzippedFiles: function (context) {
+					return context.gzippedFiles || []; // e.g. from ember-cli-deploy-gzip
+				}
+			},
 
-      upload: function(context) {
-        var self = this;
-        var credentials = this.readConfig('credentials');
-        var projectId = this.readConfig('projectId');
-        var bucket = this.readConfig('bucket');
-        var distFiles = this.readConfig('distFiles');
-        var gzippedFiles = this.readConfig('gzippedFiles');
-        var filePattern = this.readConfig('filePattern');
-        var filesToUpload = distFiles.filter(minimatch.filter(filePattern, { matchBase: true }));
+			upload: function (context) {
+				var self = this;
+				var keyFilename = this.readConfig('keyFilename');
+				var projectId = this.readConfig('projectId');
+				var bucket = this.readConfig('bucket');
+				var distFiles = this.readConfig('distFiles');
+				var gzippedFiles = this.readConfig('gzippedFiles');
+				var filePattern = this.readConfig('filePattern');
+				var filesToUpload = distFiles.filter(minimatch.filter(filePattern, {
+					matchBase: true
+				}));
 
-        this.log('uploading..');
+				this.log('uploading..');
 
-        var config = {
-          bucket: bucket,
-          fileBase: context.distDir,
-          filePaths: filesToUpload,
-          gzippedFilePaths: gzippedFiles
-        };
-        
-        if ( projectId && credentials ) {
-          config['gcloud'] = {
-            credentials: credentials,
-            projectId: projectId
-          };
-        }
+				var config = {
+					bucket: bucket,
+					fileBase: context.distDir,
+					filePaths: filesToUpload,
+					gzippedFilePaths: gzippedFiles
+				};
 
-        return upload(this, config)
-        .then(function (filesUploaded) {
-          self.log('uploaded ' + filesUploaded.length + ' files ok', { verbose: true });
-          return { filesUploaded: filesUploaded };
-        })
-        .catch(this._errorMessage.bind(this));
-      },
+				// or use GOOGLE_APPLICATION_CREDENTIALS with path to JSON service account and GCLOUD_PROJECT as projectId
+				if (projectId && keyFilename) {
+					config['gcloud'] = {
+						'keyFilename': keyFilename,
+						'projectId': projectId
+					};
+				}
 
-      _errorMessage: function(error) {
-        this.log(error, { color: 'red' });
-        if (error) {
-          this.log(error.stack, { color: 'red' });
-        }
-        return Promise.reject(error);
-      }
-    });
+				return upload(this, config)
+					.then(function (filesUploaded) {
+						self.log('uploaded ' + filesUploaded.length + ' files ok', {
+							verbose: true
+						});
+						return {
+							filesUploaded: filesUploaded
+						};
+					})
+					.catch(this._errorMessage.bind(this));
+			},
 
-    return new Plugin();
-  }
+			_errorMessage: function (error) {
+				this.log(error, {
+					color: 'red'
+				});
+				if (error) {
+					this.log(error.stack, {
+						color: 'red'
+					});
+				}
+				return Promise.reject(error);
+			}
+		});
+
+		return new Plugin();
+	}
 };
--- libs/upload.js
+++ libs/upload.js
@@ -1,40 +1,55 @@
+/* jshint node: true */
 var path = require('path');
 var gcloud = require('gcloud');
 var Promise = require('ember-cli/lib/ext/promise');
 
+
 module.exports = function uploadToGCS(plugin, config) {
-  var cloud = gcloud(config.gcloud);
-  var gcs = cloud.storage();
-  var bucket = gcs.bucket(config.bucket);
+	var cloud = gcloud(config.gcloud);
+	var gcs = cloud.storage();
+	var bucket = gcs.bucket(config.bucket);
+	var promises = [];
+	var total = config.filePaths.length;
 
-  return Promise.all(config.filePaths.map(function (filePath) {
-    var basePath = path.join(config.fileBase, filePath);
-    var isGzipped = config.gzippedFilePaths.indexOf(filePath) !== -1;
+	var upload = function (basePath, filePath, isGzipped, resolve, reject) {
+		bucket.upload(basePath, {
+			destination: filePath,
+			gzip: !isGzipped,
+			validation: 'crc32c'
+		}, function (err, file /*, apiResponse */ ) {
+			if (err) {
+				reject(err);
+			} else {
+				file.makePublic(function (err /*, res */ ) {
+					if (err) {
+						reject(err);
+					} else {
+						file.download({
+							validation: false
+						}, function (err, contents) {
+							if (err) {
+								reject(err);
+							} else {
+								plugin.log('✔  ' + filePath, {
+									verbose: true
+								});
+								resolve(contents.toString('utf8'));
+							}
+						});
+					}
+				});
+			}
+		});
+	};
 
-    return new Promise(function (resolve, reject) {
-      return bucket.upload(basePath, {
-        destination: filePath,
-        gzip: !isGzipped
-      }, function (err, file) {
-        if (err) {
-          return reject(err);
-        }
-
-        file.makePublic(function (err, res) {
-          if (err) {
-            return reject(err);
-          }
-
-          file.download({ validation: false }, function(err, contents) {
-            if (err) {
-              return reject(err);
-            }
-            
-            plugin.log('✔  ' + filePath, { verbose: true });
-            resolve(contents.toString('utf8'));
-          });
-        });
-      });
-    });
-  }));
+	for (var i = 0; i < total; i++) {
+		var filePath = config.filePaths[i];
+		var basePath = path.join(config.fileBase, filePath);
+		var isGzipped = config.gzippedFilePaths.indexOf(filePath) !== -1;
+		promises.push(new Promise(function (resolve, reject) {
+			// throttling to 20 reqeusts per second
+			setTimeout(upload.bind(null, basePath, filePath, isGzipped, resolve, reject), i * 50);
+		}));
+	}
+	return Promise.all(promises);
 };
